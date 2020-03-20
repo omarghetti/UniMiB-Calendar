cd2c4a1d2e5e651cc62e780fab751cb666af4eda
Grieco <grieco@intre.it>
2020-03-20 18:40:31 +0100

    Add new line for hooks test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
+
 const db = mongoose.connection;
 db.on('error', () => {});
 db.once('open', () => {});

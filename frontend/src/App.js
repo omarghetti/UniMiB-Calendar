d3a25968f0d69e7624f40057734943ea3db71f9b
Grieco <grieco@intre.it>
2020-03-20 16:03:40 +0100

    Add husky, prettier and lint-staged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(async () => {
-        const result = await axios(
-            '/events',
-        );
-        setData(result.data);
-        console.info({result})
-    }, []);
+  useEffect(async () => {
+    const result = await axios("/events");
+    setData(result.data);
+    console.info({ result });
+  }, []);
 
-    return (
-        <div className="App">
-            {data.map(item => (
-                <li key={item.title}>
-                    {item.title}
-                </li>
-            ))}
-        </div>
-    );
+  return (
+    <div className="App">
+      {data.map(item => (
+        <li key={item.title}>{item.title}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;

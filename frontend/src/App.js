33cbd4884c0ac6658e1613ada67a1bb8ba8284f6
Grieco <grieco@intre.it>
2020-03-20 18:29:48 +0100

    Fix test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    const result = await axios("/events");
-    setData(result.data);
-    console.info({ result });
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const result = await axios('/events');
+      setData(result.data);
+      console.info({ result });
+    };
+    fetchEvents();
   }, []);
 
   return (

9614463f40fbd87d211612eb9327c0f9e139475b
Grieco <grieco@intre.it>
2020-05-08 17:05:03 +0200

    Rename file

--- backend/server.js
+++ backend/server.js
@@ -122,7 +122,7 @@ app.use('/api/events', isLoggedIn, eventsRouter);
 
 app.get('/app', isLoggedIn, (req, res) => {
   console.info('serve app');
-  res.sendFile(path.join(__dirname, 'ui', 'app', 'app.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'app', 'index.html'));
 });
 
 app.listen(PORT, HOST);

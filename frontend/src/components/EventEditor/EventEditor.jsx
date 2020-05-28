c35ffaa6cf385258801879ec72b8a1abb6ad88d7
Grieco <grieco@intre.it>
2020-05-28 17:18:08 +0200

    Update style and layout

--- frontend/src/components/EventEditor/EventEditor.jsx
+++ frontend/src/components/EventEditor/EventEditor.jsx
@@ -5,7 +5,6 @@ import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import NativeSelect from "@material-ui/core/NativeSelect";
 import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
 import Select from "@material-ui/core/Select";
@@ -15,6 +14,12 @@ import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import SaveIcon from "@material-ui/icons/Save";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import EventIcon from "@material-ui/icons/Event";
+import TimeIcon from "@material-ui/icons/AccessTime";
+import PeopleIcon from "@material-ui/icons/People";
+import PlaceIcon from "@material-ui/icons/Place";
+import NotesIcon from "@material-ui/icons/Notes";
 
 const useStyles = makeStyles(theme => ({
   header: {
@@ -37,6 +42,16 @@ const useStyles = makeStyles(theme => ({
   },
   chip: {
     margin: 2
+  },
+  fieldIcon: {
+    alignSelf: "flex-end",
+    textAlign: "center",
+    marginBottom: "4px"
+  },
+  longTextFieldIcon: {
+    alignSelf: "center",
+    textAlign: "center",
+    marginBottom: "32px"
   }
 }));
 
@@ -58,8 +73,7 @@ function EventEditor() {
     type: "",
     allDay: false,
     participants: [],
-    place: "",
-    notes: ""
+    place: ""
   });
   const [isFetching, setIsFetching] = useState(true);
   const [eventTypes, setEventTypes] = useState([]);
@@ -67,25 +81,16 @@ function EventEditor() {
 
   const history = useHistory();
 
-  const handleChange = useCallback(
-    (field, value) => {
-      setNewEvent({
-        ...newEvent,
-        [field]: value
-      });
-    },
-    [newEvent]
-  );
+  const handleChange = (field, value) => {
+    setNewEvent({
+      ...newEvent,
+      [field]: value
+    });
+  };
 
   const setFetchingCompleted = useCallback(() => {
     setIsFetching(false);
   }, []);
-
-  useEffect(() => {
-    if (eventTypes) {
-      handleChange("type", eventTypes[0]);
-    }
-  }, [eventTypes, handleChange]);
 
   useEffect(() => {
     async function fetchEventTypes() {
@@ -122,108 +127,141 @@ function EventEditor() {
       <div />
     ) : (
       <div className={classes.detail}>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="age-native-helper">Tipo</InputLabel>
-          <NativeSelect
-            value={newEvent.type}
-            onChange={event => handleChange("type", event.target.value)}
-            inputProps={{
-              name: "age",
-              id: "age-native-helper"
-            }}
-          >
-            {eventTypes.map(type => (
-              <option value={type} key={type}>
-                {type}
-              </option>
-            ))}
-          </NativeSelect>
-        </FormControl>
-        <br />
-        <br />
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <DateTimePicker
-            label="Inizio"
-            inputVariant="standard"
-            value={newEvent.start}
-            onChange={value => handleChange("start", value.toISOString())}
-          />
-        </MuiPickersUtilsProvider>
-        <br />
-        <br />
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <DateTimePicker
-            label="Fine"
-            inputVariant="standard"
-            value={newEvent.end}
-            onChange={value => handleChange("end", value.toISOString())}
-          />
-        </MuiPickersUtilsProvider>
-        <br />
-        <br />
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-mutiple-chip-label">Partecipanti</InputLabel>
-          <Select
-            labelId="demo-mutiple-chip-label"
-            id="demo-mutiple-chip"
-            multiple
-            value={newEvent.participants}
-            onChange={event => handleChange("participants", event.target.value)}
-            input={<Input id="select-multiple-chip" />}
-            renderValue={selected => (
-              <div className={classes.chips}>
-                {selected.map(value => (
-                  <Chip key={value} label={value} className={classes.chip} />
+        <Grid container spacing={3}>
+          <Grid item xs={1} className={classes.fieldIcon}>
+            <EventIcon />
+          </Grid>
+          <Grid item xs={11}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label">Tipo</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={newEvent.type}
+                onChange={event => handleChange("type", event.target.value)}
+              >
+                {eventTypes.map(type => (
+                  <MenuItem value={type} key={type}>
+                    {type}
+                  </MenuItem>
                 ))}
-              </div>
-            )}
-            MenuProps={MenuProps}
-          >
-            {userEmails.map(email => (
-              <MenuItem key={email} value={email}>
-                {email}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <br />
-        <br />
-
-        <FormControl className={classes.formControl}>
-          <TextField
-            required
-            id="standard-required"
-            label="Luogo"
-            value={newEvent.place}
-            onChange={event => handleChange("place", event.target.value)}
-          />
-        </FormControl>
-        <br />
-        <br />
-
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="standard-multiline-static"
-            label="Note"
-            multiline
-            rows={4}
-            value={newEvent.notes}
-            onChange={event => handleChange("notes", event.target.value)}
-          />
-        </FormControl>
-        <br />
-        <br />
-        <FormControl>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            className={classes.button}
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
-        </FormControl>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={1} className={classes.fieldIcon}>
+            <TimeIcon />
+          </Grid>
+          <Grid item xs={3}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <DateTimePicker
+                label="Inizio"
+                inputVariant="standard"
+                value={newEvent.start}
+                onChange={value => handleChange("start", value.toISOString())}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item xs={8}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <DateTimePicker
+                label="Fine"
+                inputVariant="standard"
+                value={newEvent.end}
+                onChange={value => handleChange("end", value.toISOString())}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item xs={1} className={classes.fieldIcon}>
+            <PeopleIcon />
+          </Grid>
+          <Grid item xs={11}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-mutiple-chip-label">Partecipanti</InputLabel>
+              <Select
+                labelId="demo-mutiple-chip-label"
+                id="demo-mutiple-chip"
+                multiple
+                value={newEvent.participants}
+                onChange={event =>
+                  handleChange("participants", event.target.value)
+                }
+                input={<Input id="select-multiple-chip" />}
+                renderValue={selected => (
+                  <div className={classes.chips}>
+                    {selected.map(value => (
+                      <Chip
+                        key={value}
+                        label={value}
+                        className={classes.chip}
+                      />
+                    ))}
+                  </div>
+                )}
+                MenuProps={MenuProps}
+              >
+                {userEmails.map(email => (
+                  <MenuItem key={email} value={email}>
+                    {email}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={1} className={classes.fieldIcon}>
+            <PlaceIcon />
+          </Grid>
+          <Grid item xs={11}>
+            <FormControl className={classes.formControl}>
+              <TextField
+                required
+                id="standard-required"
+                label="Luogo"
+                value={newEvent.place}
+                onChange={event => handleChange("place", event.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={1} className={classes.longTextFieldIcon}>
+            <NotesIcon />
+          </Grid>
+          <Grid item xs={11}>
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="standard-multiline-static"
+                label="Note"
+                multiline
+                rows={4}
+                value={newEvent.notes}
+                onChange={event => handleChange("notes", event.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <FormControl>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <Button
+                  variant="outlined"
+                  size="large"
+                  color="secondary"
+                  className={classes.button}
+                >
+                  Annulla
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className={classes.button}
+                  startIcon={<SaveIcon />}
+                >
+                  Salva
+                </Button>
+              </Grid>
+            </Grid>
+          </FormControl>
+        </Grid>
       </div>
     );
   }
